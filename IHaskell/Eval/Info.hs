0ba3c8e760070a59595cf9231a5abf1023f7ed99
Andrew Gibiansky <andrew.gibiansky@gmail.com>
2013-11-10 20:20:47 -0800

    Adding forgotten file

---
+++ IHaskell/Eval/Info.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{- | Description : Inspect type and function information and documentation.
+-}
+module IHaskell.Eval.Info (
+  info
+  ) where
+
+import ClassyPrelude hiding (liftIO)
+
+import IHaskell.Eval.Evaluate (typeCleaner, Interpreter)
+
+import GHC
+import Outputable
+import Exception
+
+info :: String -> Interpreter String
+info name = ghandle handler $ do
+  dflags <- getSessionDynFlags
+  result <- exprType name
+  return $ typeCleaner $ showPpr dflags result 
+  where 
+    handler :: SomeException -> Interpreter String
+    handler _ = return ""
