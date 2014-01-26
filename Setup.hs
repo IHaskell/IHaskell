49b2e41020bcf4b7046a1fdb8a2e5b4a619ececb
Andrew Gibiansky <andrew.gibiansky@gmail.com>
2014-01-25 21:22:15 -0800

    Setup.hs now builds profile.tar, closes #164

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,23 @@
 import Distribution.Simple
-main = defaultMain
+
+import Control.Applicative ((<$>))
+import Data.List (isInfixOf)
+import Codec.Archive.Tar (create)
+import System.Directory (getDirectoryContents)
+
+main = defaultMainWithHooks simpleUserHooks {
+    preBuild = makeProfileTar
+  }
+
+makeProfileTar args flags = do
+  putStrLn "Building profile.tar."
+
+  let profileDir = "profile"
+      tarFile = profileDir ++ "/profile.tar"
+  files <- filter realFile <$> filter notProfileTar <$> getDirectoryContents profileDir
+  print files
+  create tarFile profileDir files
+  preBuild simpleUserHooks args flags
+  where
+    notProfileTar str = not $ "profile.tar" `isInfixOf` str
+    realFile str = str /= "." && str /= ".."
