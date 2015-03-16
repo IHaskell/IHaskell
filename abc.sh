2409525e95a1b722d5187c1395552208fd46eef7
Vladimir Alekseichenko <vova@vova.me>
2015-03-16 06:22:09 +0100

    removed temporary file

--- abc.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash sh
-
-if hash ihaskell 2>/dev/null; then
-  ihaskell install 2>/dev/null || echo "\"ihaskell install\" - failed, required ipython --version == 3.0.0 is $(ipython --version)"
-else
-  echo "tip, don't forget run: ihaskell install"
-fi
