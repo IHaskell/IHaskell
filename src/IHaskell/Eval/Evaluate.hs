7e197132c888ae6db4cb212f4f4159cfac085d34
Andrew Gibiansky <andrew.gibiansky@gmail.com>
2015-04-07 13:13:02 -0700

    GHC 7.10 fix due to GHC API change

--- src/IHaskell/Eval/Evaluate.hs
+++ src/IHaskell/Eval/Evaluate.hs
@@ -183,8 +183,9 @@ initializeImports = do
                              pkg <- db
                              depId <- depends pkg
                              dep <- filter ((== depId) . installedPackageId) db
-                             guard
-                               (iHaskellPkgName `isPrefixOf` packageIdString (packageConfigId dep))
+                             let idString = packageIdString' dflags (packageConfigId dep)
+                             guard (iHaskellPkgName `isPrefixOf` idString)
+                                    
 
                            -- ideally the Paths_ihaskell module could provide a way to get the hash too
                            -- (ihaskell-0.2.0.5-f2bce922fa881611f72dfc4a854353b9), for now. Things will end badly if you also
