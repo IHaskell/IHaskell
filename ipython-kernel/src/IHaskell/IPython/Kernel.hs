a04b191da8cc465f17f1ea47c73deb21cbfa7e0b
Andrew Gibiansky <andrew.gibiansky@gmail.com>
2016-08-18 21:00:54 -0700

    Remove the ipython-kernel package.
    It is being replaced by the new jupyter package. Hooray, big refactor!

--- ipython-kernel/src/IHaskell/IPython/Kernel.hs
+++
@@ -1,9 +0,0 @@
--- | This module exports all the types and functions necessary to create an IPython language kernel
--- that supports the @ipython console@ and @ipython notebook@ frontends.
-module IHaskell.IPython.Kernel (module X) where
-
-import           IHaskell.IPython.Types as X
-import           IHaskell.IPython.Message.Writer as X
-import           IHaskell.IPython.Message.Parser as X
-import           IHaskell.IPython.Message.UUID as X
-import           IHaskell.IPython.ZeroMQ as X
