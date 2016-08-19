ea7e6ca045ee0e7600e2aeb574934ddc583a1809
Andrew Gibiansky <andrew.gibiansky@gmail.com>
2016-08-18 21:50:43 -0700

    Get rid of ghc-parser: we only support GHC 7.10 now.

--- ghc-parser/Setup.hs
+++
@@ -1,7 +0,0 @@
-import           Distribution.Simple
-import           System.Process
-
-main = defaultMainWithHooks
-         simpleUserHooks { preConf = \args confFlags -> do
-           system "./build-parser.sh"
-           preConf simpleUserHooks args confFlags }
