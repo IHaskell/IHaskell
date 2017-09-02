8ae01dac6a2fc2936aa06e6ec7875041f0c60c9a
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-09-02 10:13:24 +0700

    Use defaultMain

--- ghc-parser/Setup.hs
+++ ghc-parser/Setup.hs
@@ -1,7 +1,3 @@
 import           Distribution.Simple
-import           System.Process
 
-main = defaultMainWithHooks
-         simpleUserHooks { preConf = \args confFlags -> do
-           system "./build-parser.sh"
-           preConf simpleUserHooks args confFlags }
+main = defaultMain
