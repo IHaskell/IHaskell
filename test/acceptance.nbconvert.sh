ee5c008501811dfd333742f5184b1bf0f4708149
James Brock <jamesbrock@gmail.com>
2019-06-05 10:36:11 +0900

    Stable nbconvert acceptance test
    Change the `nbconvert` test so that it converts a notebook with no
    Hoogle (which changes over time) and no images (which differ on
    different systems).
    
    This should reduce the number of surprise Travis test failures.

---
+++ test/acceptance.nbconvert.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+# Run nbconvert acceptance tests
+# ------------------------------
+#
+# This script must be called from the root directory of IHaskell.
+#
+# Positional arguments to this script are the invocation for `nbconvert`.
+# For example:
+#
+# Invoke from the the root IHaskell directory:
+#
+#    test/acceptance.nbconvert.sh jupyter nbconvert
+#
+# Invoke with `stack` from the the root IHaskell directory:
+#
+#    test/acceptance.nbconvert.sh stack exec -- jupyter nbconvert
+#
+# Invoke with Stack+Docker from the the root IHaskell directory:
+#
+#    test/acceptance.nbconvert.sh stack --docker exec -- jupyter nbconvert
+#
+# Invoke with Nix from the root IHaskell directory:
+#
+#    test/acceptance.nbconvert.sh result/bin/ihaskell-nbconvert
+#
+
+set -euo pipefail
+
+$* --to=notebook --execute --allow-errors --stdout test/acceptance.nbconvert.in.ipynb > test/acceptance.nbconvert.out.ipynb
+
+diff <(grep -v 'version' test/acceptance.nbconvert.in.ipynb) <(grep -v 'version' test/acceptance.nbconvert.out.ipynb)
+
