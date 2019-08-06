d199d68bd3ed98127e7d0b4795930ddd1970ede3
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-08-06 19:30:25 -0400

    release*.nix: remove default compiler and remove redundant expressions

--- release-8.2.nix
+++
@@ -1,8 +0,0 @@
-{ compiler ? "ghc822"
-, nixpkgs ? import <nixpkgs> {}
-, packages ? (_: [])
-, pythonPackages ? (_: [])
-, rtsopts ? "-M3g -N2"
-, systemPackages ? (_: [])
-}:
-  import (./release.nix) { inherit compiler nixpkgs packages pythonPackages rtsopts systemPackages; }
