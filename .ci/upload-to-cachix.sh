5c3417649b36bb583a31809458c27ee21e0773b5
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-11-24 17:17:43 -0500

    .travis.yml: use cachix --watch-store

--- .ci/upload-to-cachix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -eu
-set -f # disable globbing
-export IFS=' '
-
-echo "Uploading paths" $OUT_PATHS
-exec nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/tarball/nixos-19.09 -p cachix --run 'cachix push ihaskell $OUT_PATHS'
