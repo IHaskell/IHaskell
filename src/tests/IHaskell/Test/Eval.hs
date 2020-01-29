a54b2a0f4a95a110660a8f3446fa49c9ca2f0492
Vaibhav Sagar <vaibhavsagar@gmail.com>
2020-01-28 23:24:35 -0500

    Add tests for :sprint

--- src/tests/IHaskell/Test/Eval.hs
+++ src/tests/IHaskell/Test/Eval.hs
@@ -169,6 +169,10 @@ testEval =
 #endif
       ":k Maybe" `becomes` ["Maybe :: * -> *"]
       ":in String" `pages` ["type String = [Char] \t-- Defined in \8216GHC.Base\8217"]
+      [hereLit|
+        let x = ['a'..'z']
+        :sprint x
+      |] `becomes` ["x = _"]
 
     it "captures stderr" $ do
       [hereLit|
