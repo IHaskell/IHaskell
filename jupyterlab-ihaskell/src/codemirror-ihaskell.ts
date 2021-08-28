7a4c4869c8166b2f7f8184dc75171c35cc04f7df
Vaibhav Sagar <vaibhavsagar@gmail.com>
2021-08-29 02:40:21 +1000

    jupyterlab-ihaskell: make compatible with JupyterLab 3 (#1267)

--- jupyterlab-ihaskell/src/codemirror-ihaskell.ts
+++ jupyterlab-ihaskell/src/codemirror-ihaskell.ts
@@ -1,31 +1,36 @@
-import * as CodeMirror from 'codemirror';
+// @ts-nocheck
+// The types for CodeMirror don't accept a RegExp for 'open' and 'close'
 
-import 'codemirror/lib/codemirror';
-import 'codemirror/mode/haskell/haskell';
+import { ICodeMirror, Mode } from '@jupyterlab/codemirror';
 
-CodeMirror.defineMode("ihaskell", (config) => {
-  let hmode = CodeMirror.getMode(config, "haskell");
-  return CodeMirror.multiplexingMode(
-    hmode,
-    {
-      open: /:(?=!)/, // Matches : followed by !, but doesn't consume !
-      close: /^(?!!)/, // Matches start of line not followed by !, doesn't consume character
-      mode: CodeMirror.getMode(config, "text/plain"),
-      delimStyle: "delimit"
-    },
-    {
-      open: /\[r\||\[rprint\||\[rgraph\|/,
-      close: /\|\]/ ,
-      mode: CodeMirror.getMode(config, "text/x-rsrc"),
-      delimStyle: "delimit"}
-  );
-});
+export function defineIHaskellMode({ CodeMirror }: ICodeMirror) {
+  Mode.ensure("haskell");
+  Mode.ensure("r");
+  CodeMirror.defineMode("ihaskell", (config) => {
+    let hmode = CodeMirror.getMode(config, "haskell");
+    return CodeMirror.multiplexingMode(
+      hmode,
+      {
+        open: /:(?=!)/, // Matches : followed by !, but doesn't consume !
+        close: /^(?!!)/, // Matches start of line not followed by !, doesn't consume character
+        mode: CodeMirror.getMode(config, "text/plain"),
+        delimStyle: "delimit"
+      },
+      {
+        open: /\[r\||\[rprint\||\[rgraph\|/,
+        close: /\|\]/,
+        mode: CodeMirror.getMode(config, "text/x-rsrc"),
+        delimStyle: "delimit"
+      }
+    );
+  });
 
-CodeMirror.defineMIME("text/x-ihaskell", "ihaskell");
+  CodeMirror.defineMIME("text/x-ihaskell", "ihaskell");
 
-CodeMirror.modeInfo.push({
-  ext: ['hs'],
-  mime: "text/x-ihaskell",
-  mode: 'ihaskell',
-  name: 'ihaskell'
-});
+  CodeMirror.modeInfo.push({
+    ext: ['hs'],
+    mime: "text/x-ihaskell",
+    mode: 'ihaskell',
+    name: 'ihaskell'
+  });
+}
