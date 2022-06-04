10c93054debd329a22872c93df21ece5165d74ab
Vaibhav Sagar <vaibhavsagar@gmail.com>
2022-06-05 09:54:13 +1000

    Test with NixOS-22.05

--- release-8.10.nix
+++
@@ -1,9 +0,0 @@
-{ compiler ? "ghc8107"
-, nixpkgs ? import <nixpkgs> {}
-, packages ? (_: [])
-, pythonPackages ? (_: [])
-, rtsopts ? "-M3g -N2"
-, staticExecutable ? false
-, systemPackages ? (_: [])
-}:
-  import (./release.nix) { inherit compiler nixpkgs packages pythonPackages rtsopts systemPackages; }
