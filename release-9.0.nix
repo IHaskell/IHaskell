b4c55b25660cca9ed55b50fb444b603f93a7ef5e
Vaibhav Sagar <vaibhavsagar@gmail.com>
2022-06-05 09:54:13 +1000

    Test with NixOS-22.05

--- release-9.0.nix
+++ release-9.0.nix
@@ -1,11 +1,5 @@
-let
-  nixpkgs-src = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/tarball/4869c88626382a01ed59b161afe99d9e164bd8a1";
-    sha256 = "03ncnw0p28j5jsp5dh66jl003x81jhkh2larwkq3fvsbkai4x0kn";
-  };
-in
-{ compiler ? "ghc901"
-, nixpkgs ? import nixpkgs-src {}
+{ compiler ? "ghc902"
+, nixpkgs ? import <nixpkgs> {}
 , packages ? (_: [])
 , pythonPackages ? (_: [])
 , rtsopts ? "-M3g -N2"
