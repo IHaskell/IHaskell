3ab225839f2f17f8bac2840d2698f17b3a1bfee6
thomasjm <tom@codedown.io>
2023-08-16 03:21:36 -0700

    Try fixing 8.2 eval test

--- src/tests/IHaskell/Test/Eval.hs
+++ src/tests/IHaskell/Test/Eval.hs
@@ -160,18 +160,22 @@ testEval =
                                            DisplayData PlainText "type String :: *\ntype String = [Char]\n  \t-- Defined in \8216GHC.Base\8217"
                                            , DisplayData MimeHtml "<div class=\"code CodeMirror cm-s-jupyter cm-s-ipython\"><span class=\"cm-keyword\">type</span><span class=\"cm-space\"> </span><span class=\"cm-variable-2\">String</span><span class=\"cm-space\"> </span><span class=\"cm-atom\">::</span><span class=\"cm-space\"> </span><span class=\"cm-atom\">*</span><span class=\"cm-space\"><br /></span>\n<span class=\"cm-keyword\">type</span><span class=\"cm-space\"> </span><span class=\"cm-variable-2\">String</span><span class=\"cm-space\"> </span><span class=\"cm-atom\">=</span><span class=\"cm-space\"> </span><span class=\"cm-atom\">[</span><span class=\"cm-variable-2\">Char</span><span class=\"cm-atom\">]</span><span class=\"cm-space\"><br />  \t</span><span class=\"cm-comment\">-- Defined in \8216GHC.Base\8217</span><span class=\"cm-space\"><br /></span></div>"
                                            ]]]
-
 #elif MIN_VERSION_ghc(8,4,0)
       (displays, _output) <- eval ":in String"
       displays `shouldBe` [ManyDisplay [Display [
                                            DisplayData PlainText "type String = [Char] \t-- Defined in `GHC.Base'"
                                            , DisplayData MimeHtml "<div class=\"code CodeMirror cm-s-jupyter cm-s-ipython\"><span class=\"cm-keyword\">type</span><span class=\"cm-space\"> </span><span class=\"cm-variable-2\">String</span><span class=\"cm-space\"> </span><span class=\"cm-atom\">=</span><span class=\"cm-space\"> </span><span class=\"cm-atom\">[</span><span class=\"cm-variable-2\">Char</span><span class=\"cm-atom\">]</span><span class=\"cm-space\"> \t</span><span class=\"cm-comment\">-- Defined in `GHC.Base'</span><span class=\"cm-space\"><br /></span></div>"
                                            ]]]
+#elif MIN_VERSION_ghc(8,2,0)
+      (displays, _output) <- eval ":in String"
+      displays `shouldBe` [ManyDisplay [Display [
+                                           DisplayData PlainText "type String = [Char] \t-- Defined in `GHC.Base'"
+                                           ]]]
 #else
       (displays, _output) <- eval ":in String"
       displays `shouldBe` [ManyDisplay [Display [
-                                           DisplayData PlainText "type String :: *\ntype String = [Char]\n  \t-- Defined in \8216GHC.Base\8217"]
-                                           ]]
+                                           DisplayData PlainText "type String :: *\ntype String = [Char]\n  \t-- Defined in \8216GHC.Base\8217"
+                                           ]]]
 #endif
 
     it "captures stderr" $ do
