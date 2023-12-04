64a8c90c1dc3018c62c3170617ec937cce25716a
Vaibhav Sagar <vaibhavsagar@gmail.com>
2023-12-05 01:48:00 +1100

    [WIP] Update jupyterlab-ihaskell for 4.x

--- jupyterlab-ihaskell/src/index.ts
+++ jupyterlab-ihaskell/src/index.ts
@@ -1,22 +1,29 @@
 import {
   JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { StreamLanguage, LanguageSupport } from '@codemirror/language';
+import {
+  EditorLanguageRegistry,
+  IEditorLanguageRegistry,
+} from '@jupyterlab/codemirror';
 
-import { ICodeMirror } from '@jupyterlab/codemirror';
+export const languagePlugin: JupyterFrontEndPlugin<IEditorLanguageRegistry> = {
+  id: '@jupyterlab/codemirror-extension:language-ihaskell',
+  description: 'A CodeMirror extension for IHaskell',
+  requires: [IEditorLanguageRegistry],
+  activate: (app: JupyterFrontEnd) => {
+    const languages = new EditorLanguageRegistry();
 
-import { defineIHaskellMode } from './codemirror-ihaskell';
-
-/**
- * Initialization data for the extension1 extension.
- */
-const extension: JupyterFrontEndPlugin<void> = {
-  id: 'ihaskell',
-  autoStart: true,
-  requires: [ICodeMirror],
-  activate: (app: JupyterFrontEnd, codeMirror: ICodeMirror) =>
-  {
-    defineIHaskellMode(codeMirror).catch(console.warn);
+    languages.addLanguage({
+      name: 'ihaskell',
+      mime: 'text/x-ihaskell',
+      load: async () => {
+        const hs = await Promise.resolve(import('@codemirror/legacy-modes/mode/haskell'));
+        const parser = StreamLanguage.define(hs.haskell);
+        const languageSupport = new LanguageSupport(parser);
+        return languageSupport;
+      }
+    });
+    return languages;
   }
 };
-
-export default extension;
