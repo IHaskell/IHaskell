fc1a56da59cf4bd541bd32ec8d1e27015073447e
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-04-21 19:00:02 +1000

    jupyterlab-ihaskell: works with JupyterLab 4

--- jupyterlab-ihaskell/src/index.ts
+++ jupyterlab-ihaskell/src/index.ts
@@ -3,19 +3,24 @@ import { StreamLanguage, LanguageSupport } from '@codemirror/language';
 import { IEditorLanguageRegistry } from '@jupyterlab/codemirror';
 import { haskell } from '@codemirror/legacy-modes/mode/haskell';
 
-export const languagePlugin: JupyterFrontEndPlugin<void> = {
+const plugin: JupyterFrontEndPlugin<void> = {
   id: '@jupyterlab/codemirror-extension:language-ihaskell',
+  autoStart: true,
   description: 'A CodeMirror extension for IHaskell',
   requires: [IEditorLanguageRegistry],
-  activate: (app: JupyterFrontEnd, languages: IEditorLanguageRegistry) => {
-    languages.addLanguage({
-      name: 'ihaskell',
-      mime: 'text/x-haskell',
-      load: async () => {
-        const lang = StreamLanguage.define(haskell);
-        const languageSupport = new LanguageSupport(lang);
-        return Promise.resolve(languageSupport);
-      }
-    });
+  activate: async (app: JupyterFrontEnd, languages: IEditorLanguageRegistry) => {
+    registerHaskellLanguage(languages);
   }
 };
+
+function registerHaskellLanguage(codemirrorLanguageRegistry: IEditorLanguageRegistry) {
+  const languageSupport = new LanguageSupport(StreamLanguage.define(haskell));
+  codemirrorLanguageRegistry.addLanguage({
+    name: "ihaskell",
+    mime: "text/x-ihaskell",
+    support: languageSupport,
+    extensions: ["hs"],
+  });
+}
+
+export default plugin;
--- jupyterlab-ihaskell/webpack.config.js
+++ jupyterlab-ihaskell/webpack.config.js
@@ -25,5 +25,5 @@ module.exports = {
   },
   plugins: [
     new FixupEntryPoint()
-  ]
+  ],
 }
