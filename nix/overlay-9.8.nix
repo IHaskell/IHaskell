923160c6845cfc98eb0789f408e28290f9675d91
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-05-26 23:21:13 +1000

    nix/overlay-9.8.nix: fix overrides for display packages

--- nix/overlay-9.8.nix
+++ nix/overlay-9.8.nix
@@ -7,12 +7,13 @@ sel: sup: {
           ghc-syntax-highlighter = super.ghc-syntax-highlighter_0_0_11_0.overrideScope(self: super: {
             ghc-lib-parser = self.ghc-lib-parser_9_8_2_20240223;
           });
+
           # For display libs
           diagrams-lib = sup.haskell.lib.doJailbreak super.diagrams-lib;
           svg-builder = sup.haskell.lib.doJailbreak super.svg-builder;
-          th-desugar = self.callHackage "th-desugar" "1.16" {};
-          singletons-th = super.singletons-th_3_3;
           singletons-base = self.callHackage "singletons-base" "3.3" {};
+          singletons-th = sup.haskell.lib.doJailbreak (self.callHackage "singletons-th" "3.3" {});
+          th-desugar = self.callHackage "th-desugar" "1.16" {};
           newtype-generics = sup.haskell.lib.doJailbreak super.newtype-generics;
           microlens-th = sup.haskell.lib.doJailbreak super.microlens-th;
 
