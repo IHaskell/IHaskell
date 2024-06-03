116cdedade88d8baecf33da1a677799083a4f038
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-06-03 23:33:24 +1000

    Clean up Nix expressions for 24.05 (#1501)
    * Clean up Nix expressions for 24.05

--- nix/overlay-9.6.nix
+++ nix/overlay-9.6.nix
@@ -4,13 +4,7 @@ sel: sup: {
       ghc96 = sup.haskell.packages.ghc96.override {
         overrides = self: super: {
 
-          # https://github.com/amcphail/plot/pull/23
-          plot = super.callCabal2nix "plot" (sup.fetchFromGitHub {
-            owner = "codedownio";
-            repo = "haskell-plot";
-            rev = "dfa26022b5815bcd6a5dd6c818fcd2c4d25c6d44";
-            sha256 = "1snk70l7q98cqflgaqf6l75g4hpcnf284flm9rsmk8kkzd5nnh5k";
-          }) {};
+          plot = self.callHackage "plot" "0.2.3.12" {};
 
           singletons-base = sup.haskell.lib.dontCheck (self.callHackage "singletons-base" "3.2" {});
           singletons-th = self.callHackage "singletons-th" "3.2" {};
--- nix/overlay-9.8.nix
+++ nix/overlay-9.8.nix
@@ -3,40 +3,15 @@ sel: sup: {
     packages = sup.haskell.packages // {
       ghc98 = sup.haskell.packages.ghc98.override {
         overrides = self: super: {
-
           ghc-syntax-highlighter = super.ghc-syntax-highlighter_0_0_11_0.overrideScope(self: super: {
             ghc-lib-parser = self.ghc-lib-parser_9_8_2_20240223;
           });
 
           # For display libs
-          diagrams-lib = sup.haskell.lib.doJailbreak super.diagrams-lib;
-          svg-builder = sup.haskell.lib.doJailbreak super.svg-builder;
           singletons-base = self.callHackage "singletons-base" "3.3" {};
           singletons-th = sup.haskell.lib.doJailbreak (self.callHackage "singletons-th" "3.3" {});
           th-desugar = self.callHackage "th-desugar" "1.16" {};
-          newtype-generics = sup.haskell.lib.doJailbreak super.newtype-generics;
-          microlens-th = sup.haskell.lib.doJailbreak super.microlens-th;
-
-
-          # These ones have a non-working version of gtk2hs-buildtools added via addBuildTool
-          # in Nixpkgs. Override their cabal files to remove it.
-          cairo = sup.haskell.lib.overrideCabal (sup.haskell.lib.doJailbreak super.cairo) (_: { buildTools = []; });
-          pango = sup.haskell.lib.overrideCabal (sup.haskell.lib.doJailbreak super.pango) (_: { buildTools = []; });
-          glib = sup.haskell.lib.overrideCabal (sup.haskell.lib.doJailbreak super.glib) (_: { buildTools = []; });
-
-          # https://github.com/amcphail/plot/pull/23
-          plot = super.callCabal2nix "plot" (sup.fetchFromGitHub {
-            owner = "codedownio";
-            repo = "haskell-plot";
-            rev = "dfa26022b5815bcd6a5dd6c818fcd2c4d25c6d44";
-            sha256 = "1snk70l7q98cqflgaqf6l75g4hpcnf284flm9rsmk8kkzd5nnh5k";
-          }) {};
-
-          force-layout = sup.haskell.lib.doJailbreak super.force-layout;
-          active = sup.haskell.lib.doJailbreak super.active;
-          diagrams-svg = sup.haskell.lib.doJailbreak super.diagrams-svg;
-          diagrams-cairo = sup.haskell.lib.doJailbreak super.diagrams-cairo;
-          diagrams-contrib = sup.haskell.lib.doJailbreak super.diagrams-contrib;
+          plot = self.callHackage "plot" "0.2.3.12" {};
         };
       };
     };
