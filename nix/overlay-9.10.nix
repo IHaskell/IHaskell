549009e46b8eb31a5738914a0c8e29169f8304ea
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-07-28 14:24:10 +1000

    Update `flake.lock` (#1509)
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'hls':
        'github:haskell/haskell-language-server/376f7f1802298d23aff6aa94592cd46c4d68e61b?narHash=sha256-uKckkHiXi9jlSCPFOoovQ1im0M%2BGr6mR7DMh9dR6ZBE%3D' (2024-06-29)
      → 'github:haskell/haskell-language-server/a4bcaa318e7322d12ee2b5f8a69db21525dee93f?narHash=sha256-CKlFhKvuMHPsekzeuKz8stex4R1E/ebUPSaHErtiZpk%3D' (2024-07-26)
    • Updated input 'nixpkgs24_05':
        'github:NixOS/nixpkgs/7dca15289a1c2990efbe4680f0923ce14139b042?narHash=sha256-RoxIr/fbndtuKqulGvNCcuzC6KdAib85Q8gXnjzA1dw%3D' (2024-06-30)
      → 'github:NixOS/nixpkgs/69d1c600e00f9965578c64db36b48f48d70f274e?narHash=sha256-iXGpRf1B8M%2BkG1umzSVARqHMkZKNThY1rHhgdcbPvcQ%3D' (2024-07-28)
    • Updated input 'nixpkgsMaster':
        'github:NixOS/nixpkgs/cd6602819bb0f13b32d6b923a7c46602ee6591f3?narHash=sha256-aqGMubPLZhnuHtJ/qvC0wBVI7dkiS0AqwvHyALvOk3c%3D' (2024-06-30)
      → 'github:NixOS/nixpkgs/591c4d9c8b3b16170eac11ef58ef0403e8d88c8c?narHash=sha256-PZOg7SaC3Ep8puawzsmN4kP%2BLxv4EDqUP41prbULXXw%3D' (2024-07-28)
    
    * nix/overlay-9.10.nix: update overrides
    
    * nix/overlay-9.10.nix: update overrides
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- nix/overlay-9.10.nix
+++ nix/overlay-9.10.nix
@@ -4,32 +4,24 @@ sel: sup: {
       ghc910 = sup.haskell.packages.ghc910.override {
         overrides = self: super: {
 
-          ghc-syntax-highlighter = sup.haskell.lib.doJailbreak self.ghc-syntax-highlighter_0_0_11_0;
+          ghc-syntax-highlighter = self.ghc-syntax-highlighter_0_0_12_0;
 
-          assoc = sup.haskell.lib.doJailbreak super.assoc;
           code-page = sup.haskell.lib.doJailbreak (sup.haskell.lib.dontCheck super.code-page);
-          generically = sup.haskell.lib.doJailbreak super.generically;
           primitive = sup.haskell.lib.doJailbreak super.primitive;
           call-stack = sup.haskell.lib.dontCheck super.call-stack;
           doctest = sup.haskell.lib.doJailbreak (sup.haskell.lib.dontCheck super.doctest);
           hashable = sup.haskell.lib.doJailbreak (super.hashable.overrideScope(sel: sup: {
             os-string = null;
           }));
-          unliftio-core = sup.haskell.lib.doJailbreak super.unliftio-core;
           ChasingBottoms = sup.haskell.lib.doJailbreak super.ChasingBottoms;
           th-abstraction = super.th-abstraction_0_7_0_0;
-          indexed-traversable = sup.haskell.lib.doJailbreak super.indexed-traversable;
           nothunks = sup.haskell.lib.doJailbreak super.nothunks;
           uuid-types = sup.haskell.lib.doJailbreak super.uuid-types;
-          these = sup.haskell.lib.doJailbreak super.these;
           time-compat = sup.haskell.lib.doJailbreak super.time-compat;
           scientific = sup.haskell.lib.doJailbreak super.scientific;
           quickcheck-instances = sup.haskell.lib.doJailbreak super.quickcheck-instances;
-          indexed-traversable-instances = sup.haskell.lib.doJailbreak super.indexed-traversable-instances;
-          semialign = sup.haskell.lib.doJailbreak super.semialign;
-          aeson = sup.haskell.lib.doJailbreak super.aeson_2_2_2_0;
+          aeson = sup.haskell.lib.doJailbreak super.aeson_2_2_3_0;
           integer-conversion = sup.haskell.lib.doJailbreak super.integer-conversion;
-          boring = sup.haskell.lib.doJailbreak super.boring;
           hourglass = sup.haskell.lib.dontCheck super.hourglass;
           lifted-base = sup.haskell.lib.dontCheck (sup.haskell.lib.doJailbreak super.lifted-base);
           zlib = sup.haskell.lib.doJailbreak super.zlib;
