36c8f4e528a52903a4e4a149f585dc8e855bf489
Vaibhav Sagar <vaibhavsagar@gmail.com>
2025-01-27 01:36:45 +1100

    nix/overlay-9.10.nix: update overrides

--- nix/overlay-9.10.nix
+++ nix/overlay-9.10.nix
@@ -19,7 +19,6 @@ _sel: sup: {
             os-string = null;
           }));
           ChasingBottoms = sup.haskell.lib.doJailbreak super.ChasingBottoms;
-          th-abstraction = super.th-abstraction_0_7_0_0;
           nothunks = sup.haskell.lib.doJailbreak super.nothunks;
           uuid = sup.haskell.lib.doJailbreak super.uuid;
           time-compat = sup.haskell.lib.doJailbreak super.time-compat;
