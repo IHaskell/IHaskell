49c8907dd1191e4710da3d956064aaa3d35aa2e9
Vaibhav Sagar <vaibhavsagar@gmail.com>
2025-06-05 17:43:19 +1000

    Update Nix expressions

--- nix/overlay-9.8.nix
+++ nix/overlay-9.8.nix
@@ -3,9 +3,9 @@ _sel: sup: {
     packages = sup.haskell.packages // {
       ghc98 = sup.haskell.packages.ghc98.override {
         overrides = self: super: {
-          ghc-syntax-highlighter = (self.callHackage "ghc-syntax-highlighter" "0.0.11.0" {}).overrideScope(self: _super: {
-            ghc-lib-parser = self.ghc-lib-parser_9_8_3_20241022;
-          });
+          # ghc-syntax-highlighter = (self.callHackage "ghc-syntax-highlighter" "0.0.11.0" {}).overrideScope(self: _super: {
+          #   ghc-lib-parser = self.ghc-lib-parser_9_8_3_20241022;
+          # });
 
           # For display libs
           singletons-base = self.callHackage "singletons-base" "3.3" {};
