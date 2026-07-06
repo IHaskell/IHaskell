2075f0ef03fe10166f1d0abf520f7c5b7fc8ee7c
Vaibhav Sagar <vaibhavsagar@gmail.com>
2026-07-07 02:06:10 +1000

    nix/overlay-9.*: use lib.composeExtensions (#1602)

--- nix/overlay-9.10.nix
+++ nix/overlay-9.10.nix
@@ -1,12 +1,12 @@
-_sel: sup: {
+sel: sup: {
   haskell = sup.haskell // {
     packages = sup.haskell.packages // {
-      ghc910 = sup.haskell.packages.ghc910.override {
-        overrides = self: super: {
+      ghc910 = sup.haskell.packages.ghc910.override (old: {
+        overrides = sel.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {
           ghc-lib-parser = self.callHackage "ghc-lib-parser" "9.10.3.20250912" {};
           ghc-syntax-highlighter = self.callHackage "ghc-syntax-highlighter" "0.0.12.0" {};
-        };
-      };
+        });
+      });
     };
   };
 }
--- nix/overlay-9.14.nix
+++ nix/overlay-9.14.nix
@@ -1,8 +1,8 @@
-_sel: sup: {
+sel: sup: {
   haskell = sup.haskell // {
     packages = sup.haskell.packages // {
-      ghc914 = sup.haskell.packages.ghc914.override {
-        overrides = self: super: {
+      ghc914 = sup.haskell.packages.ghc914.override (old: {
+        overrides = sel.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {
           ghc-lib-parser = self.ghc-lib-parser_9_14_1_20251220;
           ghc-lib-parser-ex = self.ghc-lib-parser-ex_9_14_2_0;
           ghc-syntax-highlighter = self.callHackage "ghc-syntax-highlighter" "0.0.14.0" {};
@@ -16,8 +16,8 @@ _sel: sup: {
             url = "https://github.com/ndmitchell/hlint/tarball/ba7b2d0a0faabc5fcf2e57ffdeffd793f2fdcc3d";
             sha256 = "sha256:0j6bng3ij9zaj1gkxdrfmq2vb2z1czii25azz49ki6vn6aqsxzbq";
           }) {};
-        };
-      };
+        });
+      });
     };
   };
 }
